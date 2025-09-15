e72b8abc961741bb9371f0e8f97a0ea23984924e
John Sjödin <sjodin.j@gmail.com>
2025-09-15 14:50:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PriceCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName;
+            double productPrice;
+            int productQuantity;
+            double totalPrice;
+
+            Console.WriteLine("Johns metodiska prisberäknare!");
+            Console.WriteLine();
+            Console.Write("Vad vill du köpa?: ");
+            productName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write($"Vad kostar {productName} per styck?: ");
+            productPrice = ValidateDoubleInput(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write($"Hur många {productName} vill du köpa?: ");
+            productQuantity = ValidateIntInput(Console.ReadLine());
+            Console.WriteLine();
+            totalPrice
+            Console.WriteLine($"Totalt att betala:");
+        }
+    }
+}
